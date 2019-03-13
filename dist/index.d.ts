261b64be95433f12472ee3417308ac852d81e957
Bernard Kim <bernardkim.seattle@gmail.com>
2019-03-12 23:57:43 -0700

    ...

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { Connection, IReconnectStrategy, ITopology } from "./Connection";
-export { Exchange, IExchangeOptions, IInitializeResult } from "./Exchange";
-export { Queue, IConsumerResult, IDeleteResult, IInitializeQueueResult, IQueueOptions } from "./Queue";
-export { Binding } from "./Binding";
-export { Message } from "./Message";
